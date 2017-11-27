4aabaa3dfb1cc71e04e162fceae2b396ba425cb3
valeria <valeria_ddd291@hotmail.com>
2017-11-27 17:07:35 -0300

    Test Rojo: Falla porque no existe la clase frame, asi que no encuantrea el metodo sumar
    Task-Url: http://127.0.0.1/redmine/issues/1

---
+++ src/test/java/Topicos/par/frame.java
@@ -0,0 +1,22 @@
+package Topicos.par;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class frame {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	public void frameTest(){
+		frame frame = new frame();
+		assertEquals(8, frame.sumar());		
+		
+	}
+
+}
--- src/test/java/Topicos/par/parcial.java
+++
@@ -1,7 +0,0 @@
-package Topicos.par;
-
-import junit.framework.TestCase;
-
-public class parcial extends TestCase {
-
-}
